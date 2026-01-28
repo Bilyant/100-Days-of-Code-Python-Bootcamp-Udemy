2c8190699072856a78ca38ebbf86a7ab6e0344ca
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2026-01-28 22:37:29 +0000

    day 8

---
+++ Day 8/01.caesar_cipher.py
@@ -0,0 +1,6 @@
+import logo
+
+print(logo.logo)
+action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+message = input("Type your message:\n")
+shift_number = input("Type the shift number:\n")
