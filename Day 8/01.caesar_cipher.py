60a420f15aff225cf3d91c21f038cc950a30de2a
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2026-02-06 13:40:42 +0000

    caesar-cipher-part-1

--- Day 8/01.caesar_cipher.py
+++ Day 8/01.caesar_cipher.py
@@ -1,6 +1,20 @@
 import logo
 
 print(logo.logo)
-action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message:\n")
-shift_number = input("Type the shift number:\n")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_amount):
+    encrypted_text = ""
+    for char in original_text:
+        index = alphabet.index(char) + shift_amount
+        index %= len(alphabet)
+        encrypted_text += alphabet[index]
+    return encrypted_text
+
+encrypted_message = encrypt(text, shift)
+
+print(f"Here is the encoded result: {encrypted_message}")
