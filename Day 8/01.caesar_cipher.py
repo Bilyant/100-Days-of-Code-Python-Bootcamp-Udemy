56c545ce58b9db9757649bca8f12a5e13b91ef75
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2026-02-07 13:25:08 +0000

    caesar-cipher-project

--- Day 8/01.caesar_cipher.py
+++ Day 8/01.caesar_cipher.py
@@ -1,20 +1,38 @@
-import logo
+import art
 
-print(logo.logo)
+def caesar(action, original_text, shift_amount):
+    message = ""
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    if action == 'decode':
+        shift_amount *= -1
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
     for char in original_text:
+        if char not in alphabet:
+            message += char
+            continue
+
         index = alphabet.index(char) + shift_amount
         index %= len(alphabet)
-        encrypted_text += alphabet[index]
-    return encrypted_text
+        message += alphabet[index]
+    print(f"Here is the {action}d result: {message}")
 
-encrypted_message = encrypt(text, shift)
+print(art.logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+is_solved = False
 
-print(f"Here is the encoded result: {encrypted_message}")
+while not is_solved:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction not in ['encode', 'decode']:
+        print("Please type a valid answer.")
+        continue
+        
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(direction, text, shift)
+
+    to_continue = input("Would you like to continue? Type 'Yes' or 'No'\n").lower()
+    if to_continue == 'no':
+        is_solved = True
+        print("Goodbye")
